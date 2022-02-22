8ad733dd82188ee2078f650e4b8544014553ed04
danielnazarian <daniel@thinknimble.com>
2022-02-21 21:10:11 -0500

    more mobs

--- Main.gd
+++ Main.gd
@@ -2,5 +2,17 @@ extends Node
 
 export (PackedScene) var mob_scene = preload("res://Mob.tscn")
 
+func _ready():
+	randomize()
+	
+	
 func _on_MobTimer_timeout():
-	var mob = 
+	var mob = mob_scene.instance()
+	
+	var mob_spawn_location = $SpawnPath/SpawnLocation
+	mob_spawn_location.unit_offset = randf()
+	
+	var player_position = $Player.transform.origin
+	
+	add_child(mob)
+	mob.initialize(mob_spawn_location.translation, player_position)
--- Mob.gd
+++ Mob.gd
@@ -10,7 +10,7 @@ func _physics_process(_delta):
 	move_and_slide(velocity)
 
 
-func initialized(start_position, player_position):
+func initialize(start_position, player_position):
 	translation = start_position
 	look_at(player_position, Vector3.UP)
 	rotate_y(rand_range(-PI/4.0, PI/4.0))
@@ -18,7 +18,6 @@ func initialized(start_position, player_position):
 	var random_speed = rand_range(min_speed, max_speed)
 	velocity = Vector3.FORWARD * random_speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
-
 
 
 func _on_VisibilityNotifier_screen_exited():

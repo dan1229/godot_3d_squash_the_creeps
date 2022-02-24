d62bc92b3f7cf9ba2530d054054a045117e9a8f3
danielnazarian <daniel@thinknimble.com>
2022-02-24 15:34:14 -0500

    1 hourish

--- Mob.gd
+++ Mob.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+signal squashed
+
 export var min_speed = 10.0
 export var max_speed = 18.0
 
@@ -20,5 +22,11 @@ func initialize(start_position, player_position):
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 
+func squash():
+	emit_signal("squashed")
+	queue_free()
+	
+
 func _on_VisibilityNotifier_screen_exited():
 	queue_free()
+	
--- Player.gd
+++ Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody
 
+signal hit
 
 export var speed = 14.0
 export var jump_impulse = 20.0
 export var fall_acceleration = 75.0
+export var bounce_impulse = 16.0
 
 var velocity = Vector3.ZERO
 
@@ -31,3 +33,21 @@ func _physics_process(delta):
 	
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
+
+	for index in range(get_slide_count()):
+		var collision = get_slide_collision(index)
+		if collision.collider.is_in_group("mob"):
+			var mob = collision.collider
+			if Vector3.UP.dot(collision.normal) > 0.1:
+				mob.squash()
+				velocity.y = bounce_impulse
+
+
+
+func die():
+	emit_signal("hit")
+	queue_free()
+	
+
+func _on_MobDetector_body_entered(body):
+	die()
